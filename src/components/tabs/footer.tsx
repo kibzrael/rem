71028a7eb5946376e3ccfb05a63cc08f1c769d54
kibzrael <kibraphael7@gmail.com>
2025-06-08 18:29:14 +0300

    continue button fix

--- src/components/tabs/footer.tsx
+++ src/components/tabs/footer.tsx
@@ -6,7 +6,7 @@ interface Props {
   disabled?: boolean;
 }
 
-const TabFooter = ({ disabled = false, children }: Props) => {
+const TabFooter = ({ disabled, children }: Props) => {
   const { step, setStep, maxStep } = useOnboarding();
   return (
     <div className="sticky bottom-0 right-0 left-0 !mt-0 -m-4 sm:-m-8 lg:-m-12 flex max-lg:flex-col lg:items-center justify-between gap-4 px-6 sm:px-10 py-6 border-t border-default bg-surface-default/60 backdrop-blur-2xl">
@@ -20,7 +20,7 @@ const TabFooter = ({ disabled = false, children }: Props) => {
         </button>
         <button
           className="flex-[3] btn"
-          disabled={step >= maxStep || disabled}
+          disabled={disabled === undefined ? step >= maxStep : disabled}
           onClick={() => setStep(step + 1)}>
           Continue
         </button>
